dd7076eefb0f50a4e08f9601dcca148793cd75be
Ethan Freestone <ejfreebie@gmail.com>
2020-10-01 13:23:07 +0100

    GORMController can now fetch a list and a particular entity

--- src/main/groovy/web_server/Controllers/PersonController.groovy
+++ src/main/groovy/web_server/Controllers/PersonController.groovy
@@ -1,19 +1,13 @@
 package web_server.controllers
 
 import web_server.domain.Person
-
 import groovy.transform.CompileStatic
 import io.micronaut.http.annotation.Controller
-import io.micronaut.http.annotation.Get
-import io.micronaut.http.annotation.Body
-import io.micronaut.http.annotation.Post
-import io.micronaut.http.annotation.Produces
-import io.micronaut.http.HttpResponse
-import io.micronaut.http.MediaType
-
-import grails.gorm.transactions.Transactional
 
 @Controller("/person")
 class PersonController extends GORMController<Person> {
 
+    PersonController() {
+        super(Person)
+    }
 }
