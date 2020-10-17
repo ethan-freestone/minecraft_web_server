744afd2c1185c4ffbae1287dc3bb22f34bd246c1
Ethan Freestone <ejfreebie@gmail.com>
2020-10-17 20:43:17 +0100

    changed dateCreated/lastUpdated granularity to 6dp from 3

--- src/main/groovy/web_server/Domain/BasicGormEntity.groovy
+++ src/main/groovy/web_server/Domain/BasicGormEntity.groovy
@@ -4,6 +4,8 @@ import groovy.transform.CompileDynamic
 import grails.gorm.annotation.Entity
 import io.micronaut.core.annotation.Introspected
 
+import java.time.LocalDateTime
+
 import com.fasterxml.jackson.annotation.JsonFormat
 
 @CompileDynamic
@@ -13,10 +15,16 @@ class BasicGormEntity {
 
     String id
 
-    @JsonFormat(locale='DEFAULT_LOCALE')
-    Date dateCreated
-    @JsonFormat(locale='DEFAULT_LOCALE')
-    Date lastUpdated
+    @JsonFormat(
+        locale='DEFAULT_LOCALE',
+        pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSSSSSSS"
+    )
+    LocalDateTime dateCreated
+    @JsonFormat(
+        locale='DEFAULT_LOCALE',
+        pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSSSSSSS"
+    )
+    LocalDateTime lastUpdated
 
     static mapping = {
         id generator: 'uuid'
